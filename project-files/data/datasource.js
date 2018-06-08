0b00300e4706d4adfce466c3af7552138939fa4f
tphdev <tphdevdrop@gmail.com>
2018-06-07 20:21:58 -0500

    fixing project-files syntax errro

--- project-files/data/datasource.js
+++ project-files/data/datasource.js
@@ -35,7 +35,7 @@ export const jobsList = [
     company: 'Gummy Mobile',
     description: `It's critical that we give 110% when proactively incentivizing stand-ups. Change the way you do business - adopt seamless industry leaders. Going forward, our knowledge transfer capability will deliver value to executive searches.`,
     years: {
-      start: '2016'
+      start: '2016',
       end: 'NOW'
     }
   },
@@ -44,7 +44,7 @@ export const jobsList = [
     company: 'Emil Labs',
     description: `So we can hit the ground running, we will be virtually engineering every bandwidth in our space. Going forward, our actionable agile workflow will deliver value to core assets. We use our mobile best practices to strategically manage our visibility expectations. We use our customer-focused stacks to intelligently manage our stakeholder expectations.`,
     years: {
-      start: '2012'
+      start: '2012',
       end: '2016'
     }
   },
@@ -53,7 +53,7 @@ export const jobsList = [
     company: 'CasaBlack',
     description: `We use our corporate capabilities to effectively manage our innovation expectations. Company-wide dot-bombs are becoming next-generation market focus experts. Conservatively reusing reliably mission critical drivers is crucial to our competitive vertical.`,
     years: {
-      start: '2011'
+      start: '2011',
       end: '2012'
     }
   },
@@ -62,7 +62,7 @@ export const jobsList = [
     company: 'Freelance',
     description: `Ethically touching base about aligning diversities will make us leaders in the long-term platform industry. Key players will take ownership of their core competencies by iteratively growing senior enterprises.`,
     years: {
-      start: '2009'
+      start: '2009',
       end: '2011'
     }
   }
